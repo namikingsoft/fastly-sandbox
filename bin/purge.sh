03c04f1fb203d196fac6bbb536e1c7deb8457cf4
Tsubasa Namiki <namikingsoft@gmail.com>
2016-06-28 00:24:49 +0900

    chore: return error code on purge script when error response

--- bin/purge.sh
+++ bin/purge.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+#!/bin/sh -e
+
 
 if [ -e .env ]; then
   source .env
 fi
 
-curl \
-  -H "Content-Type: application/json" \
-  -H "Fastly-Key: ${FASTLY_KEY}" \
-  -X POST \
-  https://api.fastly.com/service/${FASTLY_SERVICE_ID}/purge_all
+RESPONSE_JSON=$(
+  curl -s \
+    -H "Content-Type: application/json" \
+    -H "Fastly-Key: ${FASTLY_KEY}" \
+    -X POST \
+    https://api.fastly.com/service/${FASTLY_SERVICE_ID}/purge_all
+)
+
+echo $RESPONSE_JSON
+
+RESULT=$(echo $RESPONSE_JSON | jq -r .status)
+test "$RESULT" = "ok"
+
+exit $?
